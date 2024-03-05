ec60e27fcaf099dac4879649ecf18185abc3e84a
João Pedro Vitorio Santos Souza <132296221+JoaoVicks@users.noreply.github.com>
2024-03-04 21:26:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// -----------------variaveis --------------------
+const inputs = document.querySelectorAll('.input');
+const btnGerar = document.querySelector('#text-create-password span')
+
+
+//--------------------funções--------------------------
+const generateCapitalLetters = () => {
+    const letra = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+    return letra
+}
+const generateLowercase = () => {
+    const letraMin = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    return letraMin
+}
+const generateNumber = () => {
+    const numero = String.fromCharCode(Math.floor(Math.random() * 10) + 48).toString()
+    return numero
+}
+const generateSymbol = () => {
+    const codigos = '!@#$%&{}[]()'
+    const randomNumber = Math.floor(Math.random() * codigos.length)
+    return codigos[randomNumber]
+
+}
+const listaDeFunction = [generateCapitalLetters, generateLowercase, generateNumber, generateSymbol]
+
+const createPassword = () => {
+    let arrayPassword = []
+
+    for (let i = 0; i < 10; i++) {
+        const randomIndice = Math.floor(Math.random() * 4)
+        const caractere = listaDeFunction[randomIndice]()
+        arrayPassword.push(caractere)
+    }
+    console.log(arrayPassword.join(''))
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', function () {
+        this.previousElementSibling.classList.add('anima-label')
+    });
+
+    input.addEventListener('blur', function () {
+        if (input.value !== '') return
+
+        else {
+
+            this.previousElementSibling.classList.remove('anima-label')
+        }
+    });
+});
+
+//----------------------eventos--------------
+btnGerar.addEventListener('click', () => {
+ createPassword()
+})
