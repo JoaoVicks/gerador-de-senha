87e6f19c15ac30a6d5a51649d8edb93ebf7ad298
João Pedro Vitorio Santos Souza <132296221+JoaoVicks@users.noreply.github.com>
2024-03-10 08:52:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // -----------------variaveis --------------------
 const inputs = document.querySelectorAll('.input');
-const btnGerar = document.querySelector('#text-create-password span')
+const span = document.querySelector('#text-create-password span')
 const divSenha = document.querySelector('#container-create-senha')
 const pSenha = document.querySelector('#senha-gerada')
-
+const inputQuantidade = document.querySelector('#iQuantidade')
+const containerCustom = document.querySelector('#container-custom-password')
+const btnCreate = document.querySelector('.btn-create')
+const btnsCheckbox = document.querySelectorAll('.checkbox-create')
+const btnCopy = document.querySelector('#copy-password')
 //--------------------funções--------------------------
 
 const generateCapitalLetters = () => {
@@ -19,23 +23,95 @@ const generateNumber = () => {
     return numero
 }
 const generateSymbol = () => {
-    const codigos = '!@#$%&{}[]()'
+    const codigos = '!@#$%&{}[]()-+/|'
     const randomNumber = Math.floor(Math.random() * codigos.length)
     return codigos[randomNumber]
 
 }
-const listaDeFunction = [generateCapitalLetters, generateLowercase, generateNumber, generateSymbol]
+let listaDeFunction = []
 
-const createPassword = () => {
-    let arrayPassword = []
+const createPassword = (quantidade) => {
+    if (listaDeFunction.length === 0 || quantidade === '') {
+        
+        return;
+    } else {
+        let arrayPassword = "";
 
-    for (let i = 0; i < 10; i++) {
-        const randomIndice = Math.floor(Math.random() * 4)
-        const caractere = listaDeFunction[randomIndice]()
-        arrayPassword.push(caractere)
+        for (let i = 0; i < quantidade; i++) {
+            const randomIndice = Math.floor(Math.random() * listaDeFunction.length);
+
+            const caractere = listaDeFunction[randomIndice]();
+            arrayPassword += caractere;
+        }
+        
+        const senha =  arrayPassword;
+  
+        pSenha.textContent = senha;
+
+        divSenha.classList.remove('hide');
     }
-    return arrayPassword.join('')
-}
+};
+
+
+
+//----------------------eventos--------------
+span.addEventListener('click', () => {
+
+    containerCustom.classList.toggle('hide')
+})
+
+btnCreate.addEventListener('click', (e) => {
+    e.preventDefault()
+    const quantidade= inputQuantidade.value
+createPassword(quantidade)
+
+
+})
+
+
+btnsCheckbox.forEach((btn) => {
+    btn.addEventListener('change', (e) => {
+        const btn = e.target
+        switch (btn.id) {
+            case 'iNumber':
+                if (btn.checked === true) {
+                    listaDeFunction.push(generateNumber)
+                    console.log(listaDeFunction)
+                }
+                else {
+                    const indice = listaDeFunction.indexOf(generateNumber)
+                    listaDeFunction.splice(indice, 1)
+                    console.log(listaDeFunction)
+                }
+                break
+            case 'iLetras':
+                if (btn.checked === true) {
+                    listaDeFunction.push(generateLowercase, generateCapitalLetters)
+                    console.log(listaDeFunction)
+                }
+                else {
+                    const indice = listaDeFunction.indexOf(generateLowercase)
+                    listaDeFunction.splice(indice, 2)
+                    console.log(listaDeFunction)
+                }
+                break
+            case 'iSimbolos':
+                if (btn.checked === true) {
+                    listaDeFunction.push(generateSymbol)
+                    console.log(listaDeFunction)
+                }
+                else {
+                    const indice = listaDeFunction.indexOf(generateSymbol)
+                    listaDeFunction.splice(indice, 1)
+                    console.log(listaDeFunction)
+                }
+                break
+
+        }
+
+
+    })
+})
 
 inputs.forEach(input => {
     input.addEventListener('focus', function () {
@@ -52,9 +128,13 @@ inputs.forEach(input => {
     });
 });
 
-//----------------------eventos--------------
-btnGerar.addEventListener('click', () => {
-    const senha = createPassword()
-    pSenha.textContent = senha
-    divSenha.classList.remove('hide')
-}) 
+btnCopy.addEventListener('click', (e)=>{
+    e.preventDefault()
+    passwordText = pSenha.innerText
+    navigator.clipboard.writeText(passwordText).then(()=>{
+        btnCopy.innerText = 'Senha copiada com sucesso'
+        setTimeout(() => {
+            btnCopy.innerText = 'Copiar senha'
+        }, 2200);
+    })
+})
